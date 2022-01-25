4977a7af1c3ac440462c16ca79f8ad8bf6e747b6
YK Liu <cos.lyk@gmail.com>
2022-01-25 17:30:01 +0800

    Update to 6.2.2

--- Formula/qt-lite.rb
+++ Formula/qt-lite.rb
@@ -1,8 +1,8 @@
 class QtLite < Formula
   desc "Cross-platform application and UI framework"
   homepage "https://www.qt.io/"
-  url "https://download.qt.io/official_releases/qt/6.0/6.0.2/single/qt-everywhere-src-6.0.2.tar.xz"
-  sha256 "67a076640647783b95a907d2231e4f34cec69be5ed338c1c1b33124cadf10bdf"
+  url "https://download.qt.io/official_releases/qt/6.2/6.2.2/single/qt-everywhere-src-6.2.2.tar.xz"
+  sha256 "907994f78d42b30bdea95e290e91930c2d9b593f3f8dd994f44157e387feee0f"
   license all_of: ["GFDL-1.3-only", "GPL-2.0-only", "GPL-3.0-only", "LGPL-2.1-only", "LGPL-3.0-only"]
   head "https://code.qt.io/qt/qt5.git", branch: "dev", shallow: false
 
@@ -15,7 +15,6 @@ class QtLite < Formula
 
   depends_on "double-conversion"
   depends_on "freetype"
-  depends_on "glib"
   depends_on "icu4c"
   depends_on "jpeg"
   depends_on "libb2"
@@ -24,6 +23,11 @@ class QtLite < Formula
   depends_on "pcre2"
   depends_on "python@3.9"
   depends_on "zstd"
+
+  uses_from_macos "bison" => :build
+  uses_from_macos "flex"  => :build
+  uses_from_macos "gperf" => :build
+  uses_from_macos "perl"  => :build
 
   uses_from_macos "krb5"
   uses_from_macos "perl"
@@ -37,32 +41,59 @@ class QtLite < Formula
       -prefix #{HOMEBREW_PREFIX}
       -extprefix #{prefix}
 
-      -libexecdir share/qt/libexec
-      -plugindir share/qt/plugins
-      -qmldir share/qt/qml
-      -docdir share/doc/qt
-      -translationdir share/qt/translations
+      -archdatadir share/qt
+      -datadir share/qt
       -examplesdir share/qt/examples
       -testsdir share/qt/tests
 
+      -skip qt3d
       -skip qt5compat
+      -skip qtactiveqt
+      -skip qtcharts
+      -skip qtconnectivity
+      -skip qtdatavis3d
       -skip qtdoc
+      -skip qtlottie
+      -skip qtmultimedia
+      -skip qtnetworkauth
+      -skip qtpositioning
       -skip qtquick3d
       -skip qtquicktimeline
-      -skip qtshadertools
+      -skip qtremoteobjects
+      -skip qtscxml
+      -skip qtsensors
+      -skip qtserialbus
+      -skip qtserialport
+      -skip qtvirtualkeyboard
       -skip qtwayland
+      -skip qtwebchannel
+      -skip qtwebengine
+      -skip qtwebsockets
+      -skip qtwebview
 
       -libproxy
       -no-dbus
-      -no-widgets
+      -no-directfb
       -no-cups
+      -no-eglfs
+      -no-evdev
+      -no-eventfd
+      -no-journald
+      -no-glib
+      -no-gtk
+      -no-inotify
+      -no-mtdev
+      -no-pch
+      -no-sctp
+      -no-securetransport
+      -no-syslog
+      -no-tslib
+      -no-widgets
       -no-feature-relocatable
       -no-feature-concurrent
       -no-feature-libudev
       -no-feature-sql
-      -no-feature-testlib
       -no-feature-eglfs
-      -no-feature-mtdev
       -no-feature-pdf
       -no-feature-textodfwriter
       -no-feature-vnc
@@ -76,8 +107,6 @@ class QtLite < Formula
       -no-feature-quick-animatedimage
       -no-feature-quick-canvas
       -no-feature-quick-designer
-      -no-feature-assistant
-      -no-feature-designer
       -no-feature-distancefieldgenerator
       -no-feature-kmap2qmap
       -no-feature-pixeltool
@@ -85,43 +114,47 @@ class QtLite < Formula
       -no-feature-qev
       -no-feature-qtattributionsscanner
       -no-feature-qtdiag
+      -no-gif
+      -no-ico
       -system-sqlite
+      -nomake examples
+      -nomake tests
     ]
 
-    cmake_args = std_cmake_args.reject { |s| s["CMAKE_INSTALL_PREFIX"]||s["CMAKE_FIND_FRAMEWORK"] } + %W[
-      -DICU_ROOT=#{Formula["icu4c"].opt_prefix}
+    config_args << "-sysroot" << MacOS.sdk_path.to_s if OS.mac?
 
+    cmake_args = std_cmake_args(install_prefix: HOMEBREW_PREFIX, find_framework: "FIRST") + %W[
       -DCMAKE_OSX_DEPLOYMENT_TARGET=#{MacOS.version}
-      -DCMAKE_FIND_FRAMEWORK=FIRST
-
       -DINSTALL_MKSPECSDIR=share/qt/mkspecs
-      -DINSTALL_DESCRIPTIONSDIR=share/qt/modules
-
       -DFEATURE_pkg_config=ON
     ]
 
+
     system "./configure", *config_args, "--", *cmake_args
-    system "ninja"
-    system "ninja", "install"
+    system "cmake", "--build", "."
+    system "cmake", "--install", "."
 
     rm bin/"qt-cmake-private-install.cmake"
 
-    # Some config scripts will only find Qt in a "Frameworks" folder
-    frameworks.install_symlink Dir["#{lib}/*.framework"]
-
-    inreplace lib/"cmake/Qt6/qt.toolchain.cmake", /.*set.__qt_initial_.*/, ""
+    inreplace lib/"cmake/Qt6/qt.toolchain.cmake", Superenv.shims_path, ""
 
     # The pkg-config files installed suggest that headers can be found in the
     # `include` directory. Make this so by creating symlinks from `include` to
     # the Frameworks' Headers folders.
-    Pathname.glob("#{lib}/*.framework/Headers") do |path|
-      include.install_symlink path => path.parent.basename(".framework")
+    # Tracking issues:
+    # https://bugreports.qt.io/browse/QTBUG-86080
+    # https://gitlab.kitware.com/cmake/cmake/-/merge_requests/6363
+    lib.glob("*.framework") do |f|
+      # Some config scripts will only find Qt in a "Frameworks" folder
+      frameworks.install_symlink f
+      include.install_symlink f/"Headers" => f.stem
     end
 
-    mkdir libexec
-    Pathname.glob("#{bin}/*.app") do |app|
-      mv app, libexec
-      bin.write_exec_script "#{libexec/app.stem}.app/Contents/MacOS/#{app.stem}"
+    if OS.mac?
+      bin.glob("*.app") do |app|
+        libexec.install app
+        bin.write_exec_script libexec/app.basename/"Contents/MacOS"/app.stem
+      end
     end
   end
 end
